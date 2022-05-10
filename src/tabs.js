d630038a2d5e64ee7e3fb810ece69ebd5422be14
Blattchen <felix_spohn@icloud.com>
2022-05-10 15:45:40 +0200

    Finish contact page and project

--- src/tabs.js
+++ src/tabs.js
@@ -1,5 +1,8 @@
 import Pizza from "./pizza.png"
 import Burger from "./burger.png"
+import Call from "./phone.svg"
+import Map from "./map.svg"
+import Mail from "./mail.svg"
 
 const menue = () => {
     const home = document.querySelector(".home")
@@ -45,6 +48,34 @@ const contact = () => {
     if (home) {
         home.remove()
     }
+    const dom = document.getElementById("content")
+    const content = document.createElement("div")
+    const heading = document.createElement("h1")
+    const contact = document.createElement("div")
+    const phoneNumber = document.createElement("div")
+    const mailAddress = document.createElement("div")
+    const location = document.createElement("div")
+    const mapIcon = new Image()
+    const mailIcon = new Image()
+    const phoneIcon = new Image()
+    mapIcon.src = Map
+    mailIcon.src = Mail
+    phoneIcon.src = Call
+    content.classList.add("home")
+    contact.classList.add("contact")
+    heading.textContent = "Contact"
+    phoneNumber.textContent = "0123456789"
+    phoneNumber.appendChild(phoneIcon)
+    mailAddress.textContent = "example@email.com"
+    mailAddress.appendChild(mailIcon)
+    location.textContent = "examplestreet 12, example City in the World 54258"
+    location.appendChild(mapIcon)
+    contact.appendChild(phoneNumber)
+    contact.appendChild(mailAddress)
+    contact.appendChild(location)
+    content.appendChild(heading)
+    content.appendChild(contact)
+    dom.appendChild(content)
 }
 
 export { menue, contact }
